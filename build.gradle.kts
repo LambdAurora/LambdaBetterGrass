ee41d206122309cccfb7c6427c837b4fccdd12e5
LambdAurora <email@lambdaurora.dev>
2025-09-28 19:10:49 +0200

    Set compatible MC versions for 1.21.9.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@ plugins {
 base.archivesName.set(project.property("archives_base_name") as String)
 
 val mcVersion = libs.versions.minecraft.get()
-val compatibleMcVersions: Set<String> = setOf("1.21.6", "1.21.7")
+val compatibleMcVersions: Set<String> = setOf()
 val VERSION = project.property("mod_version") as String
 version = "$VERSION+$mcVersion"
 
